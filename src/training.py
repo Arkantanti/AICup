d25ffb57a0c1632ceebc3b43812ee6785cf66487
Kuba Pieczonka <kubson.pieczonka@gmail.com>
2026-03-14 13:55:58 +0100

    model: resampling functionality

--- src/training.py
+++ src/training.py
@@ -4,8 +4,10 @@ from sklearn.model_selection import StratifiedKFold
 from sklearn.utils.class_weight import compute_sample_weight
 from custom_scoring import macro_ap_xgboost
 from catboost import CatBoostClassifier
+from imblearn.over_sampling import SMOTE
+from imblearn.under_sampling import RandomUnderSampler
 
-def run_cv_training_xgboost(X, y, class_names, params=None):
+def run_cv_training_xgboost(X, y, class_names, params=None, resampling=True):
     """
     Executes Stratified K-Fold training and returns models + OOF predictions.
     Incorporates class weighting based on the 'use_weights' parameter in config.
@@ -32,6 +34,17 @@ def run_cv_training_xgboost(X, y, class_names, params=None):
     skf = StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
     models = []
 
+    name_to_id = {name: i for i, name in enumerate(class_names)}
+
+    over_names = {'Cormorants': 200, 'Ducks': 100, 'Geese': 120}
+    under_names = {'Gulls': 600}
+
+    over_strat = {name_to_id[k]: v for k, v in over_names.items() if k in name_to_id}
+    under_strat = {name_to_id[k]: v for k, v in under_names.items() if k in name_to_id}
+
+    over = SMOTE(sampling_strategy=over_strat, random_state=42)
+    under = RandomUnderSampler(sampling_strategy=under_strat, random_state=42)
+
     # 2. Compute Full Weights if requested in config
     full_weights = None
     if params.get('use_weights', False):
@@ -42,12 +55,21 @@ def run_cv_training_xgboost(X, y, class_names, params=None):
         X_train, X_val = X.iloc[train_idx], X.iloc[val_idx]
         y_train, y_val = y.iloc[train_idx], y.iloc[val_idx]
 
+        if resampling:
+            X_res, y_res = over.fit_resample(X_train, y_train)
+            X_train, y_train = under.fit_resample(X_res, y_res)
+
         model = XGBClassifier(**xgb_params)
+
+
+        current_weights = None
+        if params.get('use_weights', False):
+            current_weights = compute_sample_weight('balanced', y_train)
 
         # Apply weights only to the training subset of this fold
         fit_params = {'eval_set': [(X_val, y_val)], 'verbose': False}
         if full_weights is not None:
-            fit_params['sample_weight'] = full_weights[train_idx]
+            fit_params['sample_weight'] = current_weights
 
         model.fit(X_train, y_train, **fit_params)
         
